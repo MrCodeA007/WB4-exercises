0be3a5ef15da1a44e7ab7006d98c94cf4a80156a
Alejandro Regus <Aregus@appdev.yearup.org>
2023-05-09 15:59:05 -0400

    courses_sorting.js set and products_sorting.js set

---
+++ Sorting/products_sorting.js
@@ -0,0 +1,20 @@
+let product = [
+    { products: "Gummy Worms", prices: 5.79 },
+    { products: "Plain M&Ms", prices: 2.89 },
+    { products: "Peanut M&Ms", prices: 2.89 },
+    { products: "Swedish Fish", prices: 3.79 },
+    { products: "Fruit Snacks", prices: 2.0 },
+    { products: "snickers", prices: 1.0 },
+    { products: "jolly Ranchers pack", prices: 5.0 },
+    { products: "Crunch", prices: 2.0 },
+    { products: "Haribo", prices: 1.0 },
+    { products: "tootsie roll", prices: 0.5 },
+  ];
+
+  product.sort();
+
+let numproducts = product.length;
+for(let i = 0;i <numproducts; i++) {
+   if (product[i].products)
+}
+  
